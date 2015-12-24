43ed9883cb183f7fdbf6cb1966298cb2d53d80d7
Nate <tulanthoar@gmail.com>
2015-12-24 07:59:36 -0700

    desky update

---
+++ src/main/scala/SbtEx.scala
@@ -0,0 +1,26 @@
+import scala.language.postfixOps
+import scala.collection.mutable.LinkedHashSet
+
+/** @object App
+ *  @author ${user.name}
+ */
+object SbtEx {
+
+  def foo( x : Array[String] ) = x.foldLeft( " ="  )( ( a, b ) => a+" and "+b )
+
+  def main( args : Array[String] ) {
+    println( "Sbt Ex!" )
+    println( "concat arguments"+foo( args ) )
+    println( "Hi from envy" )
+      val set = new scala.collection.mutable.LinkedHashSet[Any]
+          var boss = new Company()
+    boss setName "company name" 
+    val pt = new BasicPoint(5, 6)
+  set += boss  // add a string
+  set += boss.getName()                // add a number
+  set += pt
+  set += main _
+    set foreach (println(_))
+  }
+
+}
