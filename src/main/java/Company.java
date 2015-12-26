18d90d25451c7e97bffb96b51455862c6f650adb
Nate <tulanthoar@gmail.com>
2015-12-26 08:14:21 -0700

    i can't remember exactly...

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,9 +1,11 @@
-    public class Company {
-        private String name;
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
+public class Company {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
